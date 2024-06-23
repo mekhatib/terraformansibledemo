a037461bf3c7e0c5aec3f32cd07b2366f1649b69
Mahil el Khatib <mahil@hashicorp.com>
2024-06-23 20:45:25 +0200

    RUN8

--- main.tf
+++ main.tf
@@ -17,10 +17,22 @@ provider "aws" {
   region = var.region
 }
 
+data "aws_vpcs" "all" {
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+data "aws_vpc" "first" {
+  id = data.aws_vpcs.all.ids[0]
+}
+
+
 resource "aws_instance" "example" {
   ami           = "ami-08a0d1e16fc3f61ea" # Replace with your desired AMI ID
   instance_type = "t2.micro"
-
+  subnet_id     = data.aws_vpc.first.default_subnet_id
   key_name = var.ssh_key_name
 
   provisioner "remote-exec" {
