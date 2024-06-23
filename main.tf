8b4561ddb5082f9cc92160a90d6c55fc7fd7b51d
Mahil el Khatib <mahil@hashicorp.com>
2024-06-23 20:32:12 +0200

    RUN6

--- main.tf
+++ main.tf
@@ -17,56 +17,26 @@ provider "aws" {
   region = var.region
 }
 
-# Data source to get the default VPC
-data "aws_vpc" "default" {
-  default = true
-}
-
-# Data source to get the subnets in the default VPC
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-# Use the first subnet ID from the list of default subnets
 resource "aws_instance" "example" {
-  ami           = "ami-08a0d1e16fc3f61ea"
+  ami           = "ami-07f92c21ece77c17d" # Replace with your desired AMI ID
   instance_type = "t2.micro"
-  subnet_id     = data.aws_subnets.default.ids[0]
 
-  tags = {
-    Name = "example-instance"
-  }
+  key_name = var.ssh_key_name
 
-   provisioner "remote-exec" {
+  provisioner "remote-exec" {
     inline = [
-      "if [ ! -f /tmp/playbook.yml ]; then",
-      "  echo '---' > /tmp/playbook.yml",
-      "  echo '- hosts: localhost' >> /tmp/playbook.yml",
-      "  echo '  tasks:' >> /tmp/playbook.yml",
-      "  echo '    - name: Ensure Apache is installed' >> /tmp/playbook.yml",
-      "  echo '      apt:' >> /tmp/playbook.yml",
-      "  echo '        name: apache2' >> /tmp/playbook.yml",
-      "  echo '        state: present' >> /tmp/playbook.yml",
-      "fi",
-      "ansible-playbook /tmp/playbook.yml"
+      "echo Hello, World!"
     ]
 
     connection {
       type        = "ssh"
-      user        = "ubuntu" # Update with your instance's user
-      private_key = file("~/.ssh/id_rsa") # Update with your private key path
+      user        = "ec2-user"
+      private_key = var.ssh_private_key
       host        = self.public_ip
     }
   }
 
-}
-output "instance_id" {
-  value = aws_instance.example.id
-}
-
-output "public_ip" {
-  value = aws_instance.example.public_ip
+  tags = {
+    Name = "ExampleInstance"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -3,3 +3,13 @@ variable "region" {
   type        = string
   description = "The AWS and HCP region to create resources in"
 }
+
+variable "ssh_key_name" {
+  type        = string
+  description = "key name to use for SSH access"
+}
+
+variable "ssh_private_key" {
+  type        = string
+  description = "The private key to use for SSH access"
+}
