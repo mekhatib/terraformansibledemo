b3c1def64707e50a00fe186cbc794c8ec9a580a4
Mahil el Khatib <mahil@hashicorp.com>
2024-06-23 23:50:35 +0200

    REWORK3

--- main.tf
+++ main.tf
@@ -4,28 +4,16 @@ provider "aws" {
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "main-vpc"
-  }
 }
 
 resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "main-subnet"
-  }
 }
 
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "main-igw"
-  }
 }
 
 resource "aws_route_table" "main" {
@@ -35,10 +23,6 @@ resource "aws_route_table" "main" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main.id
   }
-
-  tags = {
-    Name = "main-route-table"
-  }
 }
 
 resource "aws_route_table_association" "main" {
@@ -46,7 +30,7 @@ resource "aws_route_table_association" "main" {
   route_table_id = aws_route_table.main.id
 }
 
-resource "aws_security_group" "main" {
+resource "aws_security_group" "allow_ssh" {
   vpc_id = aws_vpc.main.id
 
   ingress {
@@ -62,40 +46,29 @@ resource "aws_security_group" "main" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = {
-    Name = "main-sg"
-  }
-}
-
-resource "tls_private_key" "example" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
 }
 
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
-  public_key = tls_private_key.example.public_key_openssh
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-08a0d1e16fc3f61ea" # Amazon Linux 2 AMI (HVM), SSD Volume Type
+  ami           = "ami-08a0d1e16fc3f61ea" # Amazon Linux 2 AMI
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.main.id
   key_name      = aws_key_pair.deployer.key_name
-
-  vpc_security_group_ids = [aws_security_group.main.id]
+  security_groups = [aws_security_group.allow_ssh.name]
 
   tags = {
-    Name = "web-instance"
+    Name = "WebServer"
   }
 }
 
-output "instance_public_ip" {
-  value = aws_instance.web.public_ip
+resource "aws_eip" "web_ip" {
+  instance = aws_instance.web.id
 }
 
-output "private_key_pem" {
-  value     = tls_private_key.example.private_key_pem
-  sensitive = true
+output "instance_public_ip" {
+  value = aws_eip.web_ip.public_ip
 }
