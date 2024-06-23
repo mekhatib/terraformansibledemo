6e900cff80d17f6c3c2f03ba0629337a915fc6b2
Mahil el Khatib <mahil@hashicorp.com>
2024-06-23 15:28:06 +0200

    TESTING

---
+++ main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    doormat = {
+      source  = "doormat.hashicorp.services/hashicorp-security/doormat"
+      version = "~> 0.0.6"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.8.0"
+    }
+
+    hcp = {
+      source  = "hashicorp/hcp"
+      version = "~> 0.66.0"
+    }
+  }
+}
+
+provider "doormat" {}
+
+provider "hcp" {}
+
+data "doormat_aws_credentials" "creds" {
+  provider = doormat
+  role_arn = "arn:aws:iam::${var.aws_account_id}:role/tfc-doormat-role_ansible"
+}
+
+provider "aws" {
+  region     = var.region
+  access_key = data.doormat_aws_credentials.creds.access_key
+  secret_key = data.doormat_aws_credentials.creds.secret_key
+  token      = data.doormat_aws_credentials.creds.token
+}
+
+
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-Ansible-Demo"
+  }
+}
+
+output "instance_ip" {
+  value = aws_instance.example.public_ip
+}
