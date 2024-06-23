217ecb81727265f89334525b9a19f7e35dc8fd04
Mahil el Khatib <mahil@hashicorp.com>
2024-06-23 20:02:16 +0200

    RUN4

--- main.tf
+++ main.tf
@@ -47,9 +47,9 @@ resource "aws_instance" "example" {
 
 }
 output "instance_id" {
-  value = aws_instance.web.id
+  value = aws_instance.example.id
 }
 
 output "public_ip" {
-  value = aws_instance.web.public_ip
+  value = aws_instance.example.public_ip
 }
