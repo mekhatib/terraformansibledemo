895a44f440ee539b2c341ec86f7535b593328022
Mahil el Khatib <mahil@hashicorp.com>
2024-06-24 07:30:37 +0200

    RUN100

--- main.tf
+++ main.tf
@@ -63,8 +63,8 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
   key_name      = aws_key_pair.generated_key.key_name
   subnet_id     = aws_subnet.main.id
-  security_groups = [aws_security_group.instance.name]
-
+  #security_groups = [aws_security_group.instance.name]
+  vpc_security_group_ids = [aws_security_group.instance.id]
   associate_public_ip_address = true
 
   tags = {
