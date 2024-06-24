9f905d243474c26907b93f0b0cebfbf433f78e8a
Mahil el Khatib <mahil@hashicorp.com>
2024-06-24 07:25:54 +0200

    RUN100

--- main.tf
+++ main.tf
@@ -7,30 +7,23 @@ resource "tls_private_key" "example" {
   rsa_bits  = 4096
 }
 
+resource "aws_key_pair" "generated_key" {
+  key_name   = "generated-key"
+  public_key = tls_private_key.example.public_key_openssh
+}
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "main-vpc"
-  }
 }
 
 resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "main-subnet"
-  }
 }
 
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "main-igw"
-  }
 }
 
 resource "aws_route_table" "main" {
@@ -40,10 +33,6 @@ resource "aws_route_table" "main" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main.id
   }
-
-  tags = {
-    Name = "main-route-table"
-  }
 }
 
 resource "aws_route_table_association" "main" {
@@ -51,7 +40,7 @@ resource "aws_route_table_association" "main" {
   route_table_id = aws_route_table.main.id
 }
 
-resource "aws_security_group" "allow_ssh" {
+resource "aws_security_group" "instance" {
   vpc_id = aws_vpc.main.id
 
   ingress {
@@ -67,24 +56,16 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = {
-    Name = "allow_ssh"
-  }
-}
-
-resource "aws_key_pair" "generated_key" {
-  key_name   = "generated-key"
-  public_key = tls_private_key.example.public_key_openssh
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-08a0d1e16fc3f61ea" # Amazon Linux 2 AMI (HVM), SSD Volume Type
+  ami           = "ami-08a0d1e16fc3f61ea" # Replace with your preferred AMI
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.main.id
   key_name      = aws_key_pair.generated_key.key_name
+  subnet_id     = aws_subnet.main.id
+  security_groups = [aws_security_group.instance.name]
 
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  associate_public_ip_address = true
 
   tags = {
     Name = "example-instance"
