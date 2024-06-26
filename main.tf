7111f5cbbec752744fe40f9d5aefe093c3c71962
Mahil el Khatib <mahil@hashicorp.com>
2024-06-26 13:48:00 +0200

    TIMENOW

--- main.tf
+++ main.tf
@@ -117,10 +117,18 @@ resource "aws_instance" "example" {
 
 ##############sleep for 30 seconds to allow instance to be ready##################
 
+# This resource will destroy (potentially immediately) after null_resource.next
+resource "null_resource" "previous" {}
+
 resource "time_sleep" "wait_30_seconds" {
   depends_on = [null_resource.previous]
 
   create_duration = "30s"
+}
+
+# This resource will create (at least) 30 seconds after null_resource.previous
+resource "null_resource" "next" {
+  depends_on = [time_sleep.wait_30_seconds]
 }
 
 
