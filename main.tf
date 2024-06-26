56804b000ef95e081fe61c7ece3f6bc9033f0464
Mahil el Khatib <mahil@hashicorp.com>
2024-06-26 12:01:16 +0200

    ROLLUP

--- main.tf
+++ main.tf
@@ -127,7 +127,7 @@ resource "awx_credential_machine" "example" {
 resource "awx_job_template" "baseconfig" {
   name           = "Base Service Configuration"
   job_type       = "run"
-  inventory_id   = my_inventory.id
+  inventory_id   = aap_inventory.my_inventory.id
   project_id     = "14"
   playbook       = "install_nginx.yml"
   become_enabled = true
@@ -135,7 +135,7 @@ resource "awx_job_template" "baseconfig" {
 
 
 resource "awx_job_template_credential" "baseconfig" {
-  job_template_id = baseconfig.id
+  job_template_id = awx_job_template.baseconfig.id
   credential_id   = awx_credential_machine.example.id
 }
 
