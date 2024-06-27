0e05dfbe2dbdaac9166b6ec02662f7748a8e87f7
Mahil el Khatib <mahil@hashicorp.com>
2024-06-27 10:31:01 +0200

    RUN

--- main.tf
+++ main.tf
@@ -86,6 +86,12 @@ resource "aws_security_group" "instance" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
